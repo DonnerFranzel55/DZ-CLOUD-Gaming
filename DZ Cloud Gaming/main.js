34e00a363fda98ed884a6fa0d0c6ba5b31ff7656
DonnerFranzel55 <98757222+DonnerFranzel55@users.noreply.github.com>
2022-07-16 20:33:44 +0200

    Add files via upload

---
+++ DZ Cloud Gaming/main.js
@@ -0,0 +1,12 @@
+function signin() {
+    console.log("An Error has been found");
+}
+
+/* function version() {
+    document.getElementById(version);
+
+    if version.value = "Version wählen"{
+        alert("Bitte Version wählen");
+    }
+} */
+
