a115ad1e764e98391a96b687fe0f91966d2500fa
DonnerFranzel55 <98757222+DonnerFranzel55@users.noreply.github.com>
2022-11-12 23:13:15 +0100

    added cloud gaming 1.4

---
+++ cloud_gaming/main.js
@@ -0,0 +1,17 @@
+function signin() {
+    console.log("An Error has been found");
+}
+
+function alert() {
+            
+    alert("Neuer Umbau der Seite");
+}
+
+/* function version() {
+    document.getElementById(version);
+
+    if version.value = "Version wählen"{
+        alert("Bitte Version wählen");
+    }
+} */
+
