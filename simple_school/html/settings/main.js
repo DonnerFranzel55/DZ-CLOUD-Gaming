6a24f1460c16b1e7652e03fcc89089fe48640a18
DonnerFranzel55 <98757222+DonnerFranzel55@users.noreply.github.com>
2022-11-12 23:06:10 +0100

    added master

---
+++ simple_school/html/settings/main.js
@@ -0,0 +1,30 @@
+window.onload = function dialog() {
+
+
+
+    // use fetch to read json
+    fetch('./../../../info/accounts/dzaccin.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log("from fetch", data)
+            localStorage.setItem("accounts", JSON.stringify(data));
+            [...data.user].forEach(user => {
+                if (user.name === "Devin") {
+                    fillUserDialog(user)
+                }
+            });
+
+        })
+        .catch(err => console.error(err));
+
+}
+
+function fillUserDialog(user) {
+    let name = user.name + " " + user.last_name;
+    let usid = user.userid;
+    document.getElementById("username");
+    document.getElementById("userid");
+    username.innerHTML = name;
+    userid.innerHTML = usid;
+    
+}
